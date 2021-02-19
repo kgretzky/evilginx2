f44bc3cc7e1daccf2999f7eb7b52377b731de2e8
Calum Boal <cgboal521@gmail.com>
2021-02-19 12:26:38 +0000

    Added webhook tracking integration

--- core/config.go
+++ core/config.go
@@ -1,5 +1,4 @@
 package core
-
 import (
 	"fmt"
 	"os"
@@ -42,6 +41,8 @@ type Config struct {
 	phishletNames     []string
 	activeHostnames   []string
 	redirectParam     string
+	webhookUrl        string
+	webhookParam      string
 	verificationParam string
 	verificationToken string
 	redirectUrl       string
@@ -68,6 +69,8 @@ const (
 	CFG_PROXY_PASSWORD     = "proxy_password"
 	CFG_PROXY_ENABLED      = "proxy_enabled"
 	CFG_BLACKLIST_MODE     = "blacklist_mode"
+	CFG_WEBHOOK_URL        = "webhook_url"
+	CFG_WEBHOOK_PARAM      = "webhook_param"
 )
 
 const DEFAULT_REDIRECT_URL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" // Rick'roll
@@ -121,6 +124,8 @@ func NewConfig(cfg_dir string, path string) (*Config, error) {
 	c.proxyPassword = c.cfg.GetString(CFG_PROXY_PASSWORD)
 	c.proxyEnabled = c.cfg.GetBool(CFG_PROXY_ENABLED)
 	c.blackListMode = c.cfg.GetString(CFG_BLACKLIST_MODE)
+	c.webhookUrl = c.cfg.GetString(CFG_WEBHOOK_URL)
+	c.webhookParam = c.cfg.GetString(CFG_WEBHOOK_PARAM)
 	s_enabled := c.cfg.GetStringSlice(CFG_SITES_ENABLED)
 	for _, site := range s_enabled {
 		c.sitesEnabled[site] = true
@@ -542,3 +547,4 @@ func (c *Config) GetTemplatesDir() string {
 func (c *Config) GetBlacklistMode() string {
 	return c.blackListMode
 }
+
