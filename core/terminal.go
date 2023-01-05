6f812ba8bbb408673d4f4618a3d78894bc4e994a
Brasco <a.braschi90@gmail.com>
2023-01-05 16:17:47 +0100

    help for whitelist

--- core/terminal.go
+++ core/terminal.go
@@ -983,6 +983,8 @@ func (t *Terminal) createHelp() {
 	h.AddSubCommand("config", []string{"verification_key"}, "verification_key <name>", "change name of the verification parameter in phishing url (phishing urls will need to be regenerated)")
 	h.AddSubCommand("config", []string{"verification_token"}, "verification_token <token>", "change the value of the verification token (phishing urls will need to be regenerated)")
 	h.AddSubCommand("config", []string{"redirect_url"}, "redirect_url <url>", "change the url where all unauthorized requests will be redirected to (phishing urls will need to be regenerated)")
+	h.AddSubCommand("config", []string{"country_whitelist", "add"}, "country_whitelist add <ISOCode>", "add a new country ISO Code to the list of whitelisted country which IPs are not banned")
+	h.AddSubCommand("config", []string{"country_whitelist", "delete"}, "country_whitelist delete <ISOCode>", "delete a country ISO Code from the list of whitelisted country which IPs are not banned")
 
 	h.AddCommand("proxy", "general", "manage proxy configuration", "Configures proxy which will be used to proxy the connection to remote website", LAYER_TOP,
 		readline.PcItem("proxy", readline.PcItem("enable"), readline.PcItem("disable"), readline.PcItem("type"), readline.PcItem("address"), readline.PcItem("port"), readline.PcItem("username"), readline.PcItem("password")))
