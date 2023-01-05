21ca99b9df774e3138604f51d7010ba407f3d65a
Brasco <a.braschi90@gmail.com>
2023-01-05 12:55:12 +0100

    fix

--- core/terminal.go
+++ core/terminal.go
@@ -18,9 +18,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/kgretzky/evilginx2/database"
-	"github.com/kgretzky/evilginx2/log"
-	"github.com/kgretzky/evilginx2/parser"
+	"github.com/Brasco/evilginx2/database"
+	"github.com/Brasco/evilginx2/log"
+	"github.com/Brasco/evilginx2/parser"
 
 	"github.com/chzyer/readline"
 	"github.com/fatih/color"
