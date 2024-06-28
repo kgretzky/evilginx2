ba638edc10c26c00fe3ff43d40921fc4d26b4a5a
ly-sc-0 <160992575+ly-sc-0@users.noreply.github.com>
2024-06-28 20:32:34 +0800

    Update nameserver.go
    Support CNAME and TXT records.

--- core/nameserver.go
+++ core/nameserver.go
@@ -69,17 +69,46 @@ func (o *Nameserver) handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 
 	fqdn := strings.ToLower(r.Question[0].Name)
 
+	var isCNAME = false
+
 	switch r.Question[0].Qtype {
 	case dns.TypeSOA:
 		log.Debug("DNS SOA: " + fqdn)
 		m.Answer = append(m.Answer, soa)
 	case dns.TypeA:
-		log.Debug("DNS A: " + fqdn + " = " + o.cfg.general.ExternalIpv4)
-		rr := &dns.A{
-			Hdr: dns.RR_Header{Name: fqdn, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 300},
-			A:   net.ParseIP(o.cfg.general.ExternalIpv4),
+		// Assuming o.cfg.cnameRecords is a []*CNAMERecord
+		for _, cnameRecord := range o.cfg.cnameRecords {
+			// log.Debug("CNAME Record: %+v", cnameRecord)
+			// Access Target of each individual CNAMERecord
+			if cnameRecord != nil {
+				// Access Name and Target fields of each individual CNAMERecord
+				name := cnameRecord.Name
+				target := cnameRecord.Target
+
+				if name == fqdn {
+					log.Debug("DNS CNAME: '%s' - '%s'", name, target)
+
+					// Create a DNS response CNAME record using the Name and Target fields
+					rr := &dns.CNAME{
+						Hdr: dns.RR_Header{Name: name, Rrtype: dns.TypeCNAME, Class: dns.ClassINET, Ttl: 300},
+						Target: target,
+					}
+
+					// Append the CNAME record to the Answer section of the DNS response
+					m.Answer = append(m.Answer, rr)
+					isCNAME = true
+				}
+			}
 		}
-		m.Answer = append(m.Answer, rr)
+
+		if !isCNAME {
+			log.Debug("DNS A: " + fqdn + " = " + o.cfg.general.ExternalIpv4)
+			rr := &dns.A{
+				Hdr: dns.RR_Header{Name: fqdn, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 300},
+				A: net.ParseIP(o.cfg.general.ExternalIpv4),
+			}
+			m.Answer = append(m.Answer, rr)
+		}
 	case dns.TypeNS:
 		log.Debug("DNS NS: " + fqdn)
 		if fqdn == pdom(o.cfg.general.Domain) {
@@ -91,6 +120,52 @@ func (o *Nameserver) handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 				m.Answer = append(m.Answer, rr)
 			}
 		}
+	// 20240129 https://github.com/kgretzky/evilginx2/issues/952
+	case dns.TypeTXT:
+		// Assuming o.cfg.txtRecords is a []*TXTRecord
+		for _, txtRecord := range o.cfg.txtRecords {
+			// Access TextData of each individual TXTRecord
+			if txtRecord != nil {
+				// Access Name and Value fields of each individual TXTRecord
+				name := txtRecord.Name
+				value := txtRecord.Value
+
+				log.Debug("DNS TXT: '%s' - '%s'", name, value)
+
+				// Create a DNS response TXT record using the Name and Value fields
+				rr := &dns.TXT{
+					Hdr: dns.RR_Header{Name: fqdn, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 300},
+					Txt: []string{value},
+				}
+
+				// Append the TXT record to the Answer section of the DNS response
+				m.Answer = append(m.Answer, rr)
+			}
+		}
+	// 20240220
+	case dns.TypeCNAME:
+		// Assuming o.cfg.cnameRecords is a []*CNAMERecord
+		for _, cnameRecord := range o.cfg.cnameRecords {
+			log.Debug("CNAME Record: %+v", cnameRecord)
+			// Access Target of each individual CNAMERecord
+			if cnameRecord != nil {
+				// Access Name and Target fields of each individual CNAMERecord
+				name := cnameRecord.Name
+				target := cnameRecord.Target
+
+				log.Debug("DNS CNAME: '%s' - '%s'", name, target)
+
+				// Create a DNS response CNAME record using the Name and Target fields
+				rr := &dns.CNAME{
+					Hdr: dns.RR_Header{Name: name, Rrtype: dns.TypeCNAME, Class: dns.ClassINET, Ttl: 300},
+					Target: target,
+				}
+
+				// Append the CNAME record to the Answer section of the DNS response
+				m.Answer = append(m.Answer, rr)
+			}
+		}
+
 	}
 	w.WriteMsg(m)
 }
